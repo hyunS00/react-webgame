592babcb853730e3fc37fd5b441e8f54c4e10bf8
hyunS00 <optifun72@gmail.com>
2023-05-01 22:08:38 +0900

    React-router

--- react-router/GameMatcher.jsx
+++ react-router/GameMatcher.jsx
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import NumberBaseball from "../3.숫자야구/NumberBaseballClass";
 import RSP from "../5.가위바위보/RSPClass";
 import Lotto from "../6.로또추첨/LottoClass";
+import { Route, Routes } from "react-router";
 
 class GameMatcher extends Component {
   render() {
-    if (this.props.match.params.name === "number-baseball") {
-      return <NumberBaseball />;
-    } else if (this.props.match.params.name === "RSPClass") {
-      return <RSP />;
-    } else if (this.props.match.params.name === "LottoClass") {
-      return <Lotto />;
-    }
-    return <div>일치하는 게임이 없습니다.</div>;
+    return (
+      <Routes>
+        <Route path="number-baseball" element={<NumberBaseball />} />
+        <Route path="rock-scissors-paper" element={<RSP />} />
+        <Route path="lotto-generator" element={<Lotto />} />
+        <Route path="*" element={<div>일치하는 게임이 없습니다.</div>} />
+      </Routes>
+    );
   }
 }
 
